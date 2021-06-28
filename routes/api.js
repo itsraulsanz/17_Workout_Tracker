86ea9c287402f2678948cf198151645d3fabc462
Raul Sanz <luar_znas@hotmail.com>
2021-06-28 22:24:32 +0100

    Modified server, routes and html links. Server's working

--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,31 @@
 const router = require("express").Router();
-const db = require("../models");
+const workout = require("../models/workout.js");
+
+router.post("/api/workouts", (req, res) => {});
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    { new: true, runValidators: true }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 router.get("/api/workouts", (req, res) => {
-    db.Workout.find({})
-      .sort({ date: -1 })
-      .then(workout => {
-        res.json(workout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  });
-
+  db.Workout.find({})
+    .sort({ date: -1 })
+    .then((workout) => {
+      res.json(workout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
+const db = require("./models");
+
 const app = express();
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,8 +21,15 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 // routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/html.js"));
+app.get("/workouts", (req, res) => {
+  db.workwout.find({})
+    .then(dbWorkwout => {
+      res.json(dbWorkwout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
